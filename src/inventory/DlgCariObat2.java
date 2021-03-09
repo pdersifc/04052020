16c72ef8e4be18e0d418b37c1e41cd8d0cf43183
HEWRIE <herinoid@gmail.com>
2021-03-09 13:57:01 +0700

    save racikan pemberian obat by herinoid

--- src/inventory/DlgCariObat2.java
+++ src/inventory/DlgCariObat2.java
@@ -1233,7 +1233,7 @@ private void BtnSimpanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIR
                                 single.setTotal(total);
                                 single.setKdBangsal(kdgudang.getText());
                                 if(!tbObat.getValueAt(i,13).toString().equals("")){
-                                    single.setAturanPakai(tbObat.getValueAt(i,13).toString());
+                                    single.setAturanPakai(tbObat.getValueAt(i,13).toString()==null?"":tbObat.getValueAt(i,13).toString());
                                 }
                                 detailObatList.add(single);
                             
